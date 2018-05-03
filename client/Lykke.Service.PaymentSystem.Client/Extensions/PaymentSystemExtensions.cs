902cbb111e5819fda6c60f50d24365941819abb5
Galay Mihail <m.galay@itransition.com>
2018-05-04 01:29:35 +0300

    LWW-30 PaymentSystemExtensions was remake

--- client/Lykke.Service.PaymentSystem.Client/Extensions/PaymentSystemExtensions.cs
+++ client/Lykke.Service.PaymentSystem.Client/Extensions/PaymentSystemExtensions.cs
@@ -1,13 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
-using Common.Log;
-using Lykke.Service.PaymentSystem.Client.AutorestClient;
 using Lykke.Service.PaymentSystem.Client.AutorestClient.Models;
-using Lykke.Service.PaymentSystem.Client.Extensions;
 
-namespace Lykke.Service.PaymentSystem.Client
+namespace Lykke.Service.PaymentSystem.Client.Extensions
 {
     /// <summary>
     /// PaymentSystem extensions
@@ -15,42 +9,26 @@ namespace Lykke.Service.PaymentSystem.Client
     public static class PaymentSystemExtensions
     {
         /// <summary>
-        /// GetInfo as object
+        /// GetInfo as T
         /// </summary>
-        /// <param name="src"></param>
-        /// <param name="expectedType"></param>
-        /// <param name="throwExeption"></param>
-        /// <returns></returns>
-        public static object GetInfo(this PaymentTransactionResponse src, Type expectedType = null, bool throwExeption = false)
+        /// <typeparam name="T">Return type</typeparam>
+        /// <param name="src">PaymentTransactionResponse object</param>
+        /// <returns>Deserialize object as T</returns>
+        public static T GetInfo<T>(this PaymentTransactionResponse src)
         {
             if (!PaymentSystemsAndOtherInfo.PsAndOtherInfoLinks.ContainsKey(src.PaymentSystem))
             {
-                if (throwExeption)
-                    throw new Exception("Unsupported payment system for reading other info: transactionId:" + src.Id);
-
-                return null;
+                throw new Exception("Unsupported payment system for reading other info: transactionId:" + src.Id);
             }
 
             var type = PaymentSystemsAndOtherInfo.PsAndOtherInfoLinks[src.PaymentSystem];
 
-            if (expectedType != null)
+            if (type != typeof(T))
             {
-                if (type != expectedType)
-                    throw new Exception("Payment system and Other info does not match for transactionId:" + src.Id);
+                throw new Exception("Payment system and Other info does not match for transactionId:" + src.Id);
             }
 
-            return Newtonsoft.Json.JsonConvert.DeserializeObject(src.Info, type);
-        }
-
-        /// <summary>
-        /// GetInfo as T
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="src"></param>
-        /// <returns></returns>
-        public static T GetInfo<T>(this PaymentTransactionResponse src)
-        {
-            return (T)GetInfo(src, typeof(T), true);
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(src.Info);
         }
     }
 }
