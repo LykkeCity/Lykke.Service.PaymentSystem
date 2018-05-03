4c3daf7aa2c968ac8176e1c72083fd73893f44c7
Galay Mihail <m.galay@itransition.com>
2018-05-03 13:26:19 +0300

    LWW-72 PaymentLimits was added

--- src/Lykke.Service.PaymentSystem.Services/Domain/PaymentTransaction.cs
+++ src/Lykke.Service.PaymentSystem.Services/Domain/PaymentTransaction.cs
@@ -1,25 +1,11 @@
-﻿using System;
-using Lykke.Contracts.Payments;
+﻿
+using Lykke.Service.PaymentSystem.Core.Domain;
 
 namespace Lykke.Service.PaymentSystem.Services.Domain
 {
-    public class PaymentTransaction
+    public class PaymentLimits : IPaymentLimits
     {
-        public string Id { get; set; }
-        public string ClientId { get; set; }
-        public double Amount { get; set; }
-        public string AssetId { get; set; }
-        public string WalletId { get; set; }
-        public double? DepositedAmount { get; set; }
-        public string DepositedAssetId { get; set; }
-        public double? Rate { get; set; }
-        public string AggregatorTransactionId { get; set; }
-        public DateTime Created { get; set; }
-        public PaymentStatus Status { get; set; }
-        public CashInPaymentSystem PaymentSystem { get; set; }
-        public string Info { get; set; }
-        public string OtherData { get; set; }
-        public double FeeAmount { get; set; }
-        public string MeTransactionId { get; set; }
+        public double CreditVouchersMinValue { get; set; }
+        public double CreditVouchersMaxValue { get; set; }
     }
 }
